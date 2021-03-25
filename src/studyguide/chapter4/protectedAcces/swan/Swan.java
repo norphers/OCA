7115827bd2dcc28689a8965d3fc40db15e7cab73
Oriol Canadés <oriol.canades@in2.es>
2021-03-25 19:03:37 +0100

    inhetited and non-inherited methods example

--- src/studyguide/chapter4/protectedAcces/swan/Swan.java
+++ src/studyguide/chapter4/protectedAcces/swan/Swan.java
@@ -4,10 +4,21 @@ import studyguide.chapter4.protectedAcces.shore.Bird;
 
 public class Swan extends Bird
 {
-	public void activity() {
-		Bird swan = new Bird();
-		swan.fly();
+	public void notIneritedBird() {
+		
+		Bird swan = new Bird(); 
+		//Estás instanciando un nuevo objeto Bird con
+		//referencia 'swan'.  
+		swan.fly(); //DOES NOT COMPILE
+		//no compila porquè el objecto Bird  al cual estás
+		//haciendo referencia pertenece a otro package.
+		//swan.fly() no hereda de Bird y no puede usar sus
+		//métodos.
+	}
+	
+	public void inheretedBird() {
 		fly();
+		System.out.println(birdField);
 	}
 	
 }
